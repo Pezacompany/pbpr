bb7f910fd5abb46cee4f6f9fe055c28764a6dc98
Pezacompany <pawel.peza@kontakkt.hub.pl>
2026-04-20 16:47:44 +0200

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const { username, state, charName, age, origin } = req.query;
-    const LOG_CHANNEL_ID = "1495793897451028592";
+    const DB_WEBHOOK = process.env.DB_WEBHOOK; 
 
     try {
         const userRes = await fetch(`https://users.roblox.com/v1/usernames/users`, {
@@ -9,25 +9,22 @@ export default async function handler(req, res) {
             body: JSON.stringify({ usernames: [username] })
         });
         const userData = await userRes.json();
-        if (!userData.data[0]) return res.redirect(`/?id=${state}&status=error&msg=Nie znaleziono gracza.`);
-
         const rbxId = userData.data[0].id;
+
         const profileRes = await fetch(`https://users.roblox.com/v1/users/${rbxId}`);
         const profile = await profileRes.json();
 
         if (profile.description.includes(`BCK-${state}`)) {
-            // Sygnał do bota na Discord (Baza Danych)
-            const DISCORD_WEBHOOK = process.env.DB_WEBHOOK; 
-            await fetch(DISCORD_WEBHOOK, {
+            await fetch(DB_WEBHOOK, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    content: `DATA_ID|DC:${state}|RBX:${rbxId}|SLOT:1|P1[${charName}|${age}|${origin}|Obywatel / Turysta]|P2[Brak|N/A|N/A|Brak]|EXIT:null`
+                    content: `DATA_ID|DC:${state}|RBX:${rbxId}|SLOT:1|P1[${charName}|${age}|${origin}|Obywatel]|P2[Brak|N/A|N/A|Brak]|EXIT:null`
                 })
             });
             return res.redirect(`/?status=success`);
         } else {
-            return res.redirect(`/?id=${state}&status=error&msg=Błędny kod w Bio.`);
+            return res.redirect(`/?id=${state}&status=error&msg=Kod nie pasuje!`);
         }
-    } catch (e) { res.redirect(`/?id=${state}&status=error&msg=Błąd API.`); }
+    } catch (e) { res.redirect(`/?status=error&msg=Blad API`); }
 }
